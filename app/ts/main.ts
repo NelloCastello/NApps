eee799f5fa68373666ae0045757ff445abb607df
Nello Castello <dfarino46@gmail.com>
2021-09-02 22:22:26 +0300

    Событие кнопок формы регистрации // JS

--- app/ts/main.ts
+++ app/ts/main.ts
@@ -1,16 +1,118 @@
-export {}
+export {};
 
-import $ from "jquery"
+import $ from "jquery";
 
-console.log($('.register'))
 
-$('.header__reg-link').on('click', function(): any {
-    $('.register').removeClass('register--hidden')
-})
-$('.header__auth-link').on('click', function(): any {
-    $('.auth').removeClass('auth--hidden')
-})
-$('.login__close-btn').on('click', function(): any {
-    $('.register').addClass('register--hidden')
-    $('.auth').addClass('auth--hidden')
-})
+
+// Показать/скрыть "логин"
+
+$('.header__reg-link').on('click', function() {
+    $('.register').removeClass('register--hidden');
+});
+$('.header__auth-link').on('click', function() {
+    $('.auth').removeClass('auth--hidden');
+});
+$('.login__close-btn').on('click', function() {
+    $('.register').addClass('register--hidden');
+    $('.auth').addClass('auth--hidden');
+});
+
+
+
+// Регистрация
+
+interface RegisterInterface {
+    name: string;
+    HTMLElem: JQuery;
+    HTMLElemBtn: JQuery;
+    HTMLElemBtnPrev: boolean;
+};
+
+let registerBtnPrevElem: JQuery = $('#js-register__prevBtn');
+
+let registerList: RegisterInterface[] = [
+    {
+        name: "nickname",
+        HTMLElem: $('#js-register__nicknameInput'),
+        HTMLElemBtn: $('#js-register__nextBtn'),
+        HTMLElemBtnPrev: false
+    },
+    {
+        name: "email",
+        HTMLElem: $('#js-register__emailInput'),
+        HTMLElemBtn: $('#js-register__nextBtn'),
+        HTMLElemBtnPrev: true
+    },
+    {
+        name: "code",
+        HTMLElem: $('#js-register__codeInput'),
+        HTMLElemBtn: $('#js-register__nextBtn'),
+        HTMLElemBtnPrev: true
+    },
+    {
+        name: "passwd1",
+        HTMLElem: $('#js-register__passwd1Input'),
+        HTMLElemBtn: $('#js-register__nextBtn'),
+        HTMLElemBtnPrev: true
+    },
+    {
+        name: "passwd2",
+        HTMLElem: $('#js-register__passwd2Input'),
+        HTMLElemBtn: $('#js-register__doneBtn'),
+        HTMLElemBtnPrev: true
+    }
+];
+
+let registerInputActive: number = 0;
+
+let getRegisterElemActive = function(active: number): RegisterInterface {
+    let result: RegisterInterface;
+
+    registerList.forEach(function(item: RegisterInterface) {
+        if (item.name === registerList[active].name) {
+            result = item;
+            return;
+        }
+    });
+
+    return result;
+};
+
+getRegisterElemActive(registerInputActive).HTMLElemBtn.removeClass('login__btn--hidden');
+getRegisterElemActive(registerInputActive).HTMLElem.removeClass('login__input--hidden');
+
+$('.login__btn:not(#js-register__prevBtn)').on('click', function() {
+    registerInputActive++;
+
+    console.log(registerInputActive);
+
+    if(getRegisterElemActive(registerInputActive).HTMLElemBtnPrev === true) {
+        registerBtnPrevElem.removeClass('login__btn--hidden');
+    } else {
+        registerBtnPrevElem.addClass('login__btn--hidden');
+    }
+
+    getRegisterElemActive(registerInputActive-1).HTMLElemBtn.addClass('login__btn--hidden');
+    getRegisterElemActive(registerInputActive-1).HTMLElem.addClass('login__input--hidden');
+
+    getRegisterElemActive(registerInputActive).HTMLElemBtn.removeClass('login__btn--hidden');
+    getRegisterElemActive(registerInputActive).HTMLElem.removeClass('login__input--hidden');
+});
+
+registerBtnPrevElem.on('click', function() {
+    registerInputActive--;
+
+    console.log(registerInputActive);
+
+    if(getRegisterElemActive(registerInputActive).HTMLElemBtnPrev === true) {
+        registerBtnPrevElem.removeClass('login__btn--hidden');
+    } else {
+        registerBtnPrevElem.addClass('login__btn--hidden');
+    }
+
+    getRegisterElemActive(registerInputActive+1).HTMLElemBtn.addClass('login__btn--hidden');
+    getRegisterElemActive(registerInputActive+1).HTMLElem.addClass('login__input--hidden');
+
+    getRegisterElemActive(registerInputActive).HTMLElemBtn.removeClass('login__btn--hidden');
+    getRegisterElemActive(registerInputActive).HTMLElem.removeClass('login__input--hidden');
+});
